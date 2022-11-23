77726f19e5ae4ecde4b317e333d10fd2d9c85e5b
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-11-23 11:54:04 +0530

    Update check_color_from_pair_number.py

--- check_color_from_pair_number.py
+++ check_color_from_pair_number.py
@@ -1,15 +1,13 @@
-
-import main
-
+import print_reference_module as pm
 def get_color_from_pair_number(pair_number):
     zero_based_pair_number = pair_number - 1
-    major_index = zero_based_pair_number // len(main.MINOR_COLORS)
-    if major_index >= len(main.MAJOR_COLORS):
+    major_index = zero_based_pair_number // len(pm.MINOR_COLORS)
+    if major_index >= len(pm.MAJOR_COLORS):
         raise Exception('Major index out of range')
-    minor_index = zero_based_pair_number % len(main.MINOR_COLORS)
-    if minor_index >= len(main.MINOR_COLORS):
+    minor_index = zero_based_pair_number % len(pm.MINOR_COLORS)
+    if minor_index >= len(pm.MINOR_COLORS):
         raise Exception('Minor index out of range')
-    return main.MAJOR_COLORS[major_index], main.MINOR_COLORS[minor_index]
+    return pm.MAJOR_COLORS[major_index], pm.MINOR_COLORS[minor_index]
 
 
 def test_number_to_pair(pair_number,
